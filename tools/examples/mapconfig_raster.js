555e5a8b23953fbbb9887ccc4691460c86166c49
Sandro Santilli <strk@keybit.net>
2014-07-16 12:44:18 +0200

    Add prescaling, clipping and overviews parameters in config
    Also add example MapConfig with raster usage

---
+++ tools/examples/mapconfig_raster.js
@@ -0,0 +1,10 @@
+{"version":"1.2.0",
+	"layers":[{
+    "type":"mapnik",
+	  "options":{
+      "sql":"select * from hyp_hr_of",
+	    "cartocss":"#layer { raster-opacity:1; }",
+      "cartocss_version": "2.1.1"
+    }
+  }]
+}
